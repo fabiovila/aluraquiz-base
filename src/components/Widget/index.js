35cfea8fc6e24c95a6ace325d772f2982cbccc14
fabiovila <vilamail-spam@yahoo.com.br>
2021-01-25 22:35:19 -0300

    first commit

---
+++ src/components/Widget/index.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 5px solid ${({ theme }) => theme.colors.primary};
+  -moz-border-image: -moz-linear-gradient(top, ${({ theme }) => theme.colors.primary} 30%, ${({ theme }) => theme.colors.mainBg} 80%); 
+	-webkit-border-image: -webkit-linear-gradient(top, ${({ theme }) => theme.colors.primary} 30%, ${({ theme }) => theme.colors.mainBg} 80%); 
+	border-image: linear-gradient(to bottom, ${({ theme }) => theme.colors.primary} 30%, ${({ theme }) => theme.colors.mainBg} 80%);
+	border-image-slice: 1;
+  background-color: ${({ theme }) => {
+    return theme.colors.mainBg;
+  }};
+  /* border-radius: 5px; */
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1.2;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.2;
+  }
+`;
+
+Widget.Splash = styled.div`
+background-image: url(${({ splashimg }) => splashimg });
+background-size: cover;
+width: 50px;
+height: 50px;
+position:relative;
+top: -15px;
+`
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  
+  * {
+    margin: 0;
+  }
+`;
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+export default Widget;
