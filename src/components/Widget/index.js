700baa904cfb0338cc951e462dd878cf609e4cc4
fabiovila <vilamail-spam@yahoo.com.br>
2021-01-27 19:31:44 -0300

    ...

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -11,6 +11,7 @@ const Widget = styled.div`
   background-color: ${({ theme }) => theme.colors.mainBg};
   /* border-radius: 5px; */
   overflow: hidden;
+  box-shadow: 5px 5px 5px #00000099;
 
   h1, h2, h3 {
     font-size: 16px;
